a3f341eb65b8c7d30380043770775cea156e527c
Michel Fournier <michel.fournier.trad@gmail.com>
2020-08-19 19:44:07 -0400

    updating projects.py

--- projects.py
+++ projects.py
@@ -2,11 +2,7 @@ class Project:
 
     def __init__(self, name):
         self.name = name
-<<<<<<< HEAD
-        self.list_of_occurences = []
-=======
         self.list_of_occurrences = []
->>>>>>> v3wGUI
 
     def get_name(self):
         return self.name
@@ -20,15 +16,10 @@ class Project:
     def total_words_month(self):
         # might need a date filter
         sum_words = 0
-<<<<<<< HEAD
-        for i in self.list_of_occurences:
-            sum_words += i[1]
-=======
         for i in self.list_of_occurrences:
             try:
                 sum_words += int(i[1])
             except ValueError:
                 pass
 
->>>>>>> v3wGUI
         return sum_words
